1ee96b6ece6fb8da9478fd1dc406eb8a82d53ce7
hppod <hppodgornik@gmail.com>
2020-08-15 09:44:00 -0300

    Realizando a requisição para a API e chamando a função para adicionar o resultado ao banco de dados

--- app/job/request.js
+++ app/job/request.js
@@ -1,11 +1,26 @@
 const request = require('request')
+const { endpoint_caso } = require('./endpoints.consts')
+const db = require('./database')
 
-function makeRequest_Caso(callback) {
-    request('http://www.google.com.br', (error, response, body) => {
+function makeRequest_Caso(callback, next) {
+    console.log(`Requisitando dados para ${endpoint_caso}`)
+    request(endpoint_caso, (error, response, body) => {
         if (error) {
+            console.log('Houve um erro ao processar')
             callback(error)
+            next()
         } else {
-            callback(response)
+            let firstPos = JSON.parse(body)
+            firstPos = firstPos['results'][1]
+            console.log('Dado recuperado com sucesso')
+            db.insertData_Caso(firstPos, (result, error) => {
+                if (error) {
+                    callback(error)
+                    next()
+                } else {
+                    callback(result)
+                }
+            })
         }
     })
 }
