c4209fc66e9a7f273078d04770a974f05bddbb42
hppod <hppodgornik@gmail.com>
2020-08-15 09:42:46 -0300

    Setando a propriedade logging do Sequelize como False

--- config/database.js
+++ config/database.js
@@ -6,5 +6,6 @@ module.exports = {
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  dialect: process.env.DB_DIALECT
+  dialect: process.env.DB_DIALECT,
+  logging: false
 }
