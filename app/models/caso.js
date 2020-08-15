20020db69114cdef81544462f4ae28593939d8d9
hppod <hppodgornik@gmail.com>
2020-08-15 09:43:25 -0300

    Atualizando a model CASO

--- app/models/caso.js
+++ app/models/caso.js
@@ -1,20 +1,71 @@
 module.exports = (sequelize, DataTypes) => {
     const Caso = sequelize.define('Caso', {
-        id: DataTypes.INTEGER,
-        city: DataTypes.STRING,
-        city_ibge_code: DataTypes.STRING,
-        confirmed: DataTypes.INTEGER,
-        confimed_per_100k_inhabitants: DataTypes.DOUBLE,
-        date: DataTypes.DATE,
-        death_rate: DataTypes.DOUBLE,
-        deaths: DataTypes.INTEGER,
-        estimated_population_2019: DataTypes.INTEGER,
-        is_last: DataTypes.BOOLEAN,
-        order_for_place: DataTypes.INTEGER,
-        place_type: DataTypes.STRING,
-        state: DataTypes.STRING,
-        createdAt: DataTypes.DATE
-    })
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER
+        },
+        city: {
+            allowNull: true,
+            type: DataTypes.STRING
+        },
+        city_ibge_code: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        confirmed: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        confirmed_per_100k_inhabitants: {
+            allowNull: false,
+            type: DataTypes.DOUBLE
+        },
+        date: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+        death_rate: {
+            allowNull: false,
+            type: DataTypes.DOUBLE
+        },
+        deaths: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        estimated_population_2019: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        is_last: {
+            allowNull: false,
+            type: DataTypes.BOOLEAN
+        },
+        order_for_place: {
+            allowNull: false,
+            type: DataTypes.INTEGER
+        },
+        place_type: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        state: {
+            allowNull: false,
+            type: DataTypes.STRING
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        }
+    },
+        {
+            freezeTableName: true,
+            tableName: 'CASO',
+            timestamps: true,
+            updatedAt: false
+        }
+    )
 
     return Caso
 }
