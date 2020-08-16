29c504dc408e6c1056b9ffb3ca00fb7d5f62de8b
Hygor Podgornik <hppodgornik@gmail.com>
2020-08-16 15:13:06 -0300

    Modelando a função de acordo com o MAP

--- app/job/request.js
+++ app/job/request.js
@@ -2,49 +2,62 @@ const request = require('request')
 const { endpoint_caso, endpoint_caso_islast } = require('./endpoints.consts')
 const db = require('./database')
 
-function makeRequest_Caso(callback, next) {
-    const date = new Date().toISOString()
+function makeRequest_Caso(URL = endpoint_caso, callback, next) {
 
-    db.updateOldData_Caso((result, error) => {
+    db.hasData_Caso((result, error) => {
         if (error) {
             callback(error)
             next()
         } else {
-            callback(result)
+            URL = result ? endpoint_caso_islast : endpoint_caso
+            const isLast = result
+
+            request(URL, (error, response, body) => {
+                if (error) {
+                    console.log('Houve um erro ao processar')
+                    callback(error)
+                    next()
+                } else {
+                    if (isLast) {
+                        let dateFirstPos = JSON.parse(body)
+                        dateFirstPos = dateFirstPos['results'][0]['date']
+                    }
+                }
+            })
         }
+
+        // db.hasData_Caso((count, error) => {
+        //     if (error) {
+        //         callback(error)
+        //         next()
+        //     } else {
+        //         const URL = count > 0 ? endpoint_caso_islast : endpoint_caso
+        //         const isLast = URL == endpoint_caso_islast ? true : false
+
+        //         console.log(`Requisitando dados para ${URL}`)
+
+        //         request(URL, (error, response, body) => {
+        //             if (error) {
+        //                 console.log('Houve um erro ao processar')
+        //                 callback(error)
+        //                 next()
+        //             } else {
+        //                 let firstPos = JSON.parse(body)
+        //                 firstPos = firstPos['results'].splice(0, 2)
+        //                 console.log('Dados recuperado com sucesso')
+        //                 db.insertData_Caso(firstPos, (result, error) => {
+        //                     if (error) {
+        //                         callback(error)
+        //                         next()
+        //                     } else {
+        //                         callback(result)
+        //                     }
+        //                 })
+        //             }
+        //         })
+        //     }
+        // })
     })
-
-    // db.hasData_Caso((count, error) => {
-    //     if (error) {
-    //         callback(error)
-    //         next()
-    //     } else {
-    //         const URL = count > 0 ? endpoint_caso_islast : endpoint_caso
-    //         const isLast = URL == endpoint_caso_islast ? true : false
-
-    //         console.log(`Requisitando dados para ${URL}`)
-
-    //         request(URL, (error, response, body) => {
-    //             if (error) {
-    //                 console.log('Houve um erro ao processar')
-    //                 callback(error)
-    //                 next()
-    //             } else {
-    //                 let firstPos = JSON.parse(body)
-    //                 firstPos = firstPos['results'].splice(0, 2)
-    //                 console.log('Dados recuperado com sucesso')
-    //                 db.insertData_Caso(firstPos, (result, error) => {
-    //                     if (error) {
-    //                         callback(error)
-    //                         next()
-    //                     } else {
-    //                         callback(result)
-    //                     }
-    //                 })
-    //             }
-    //         })
-    //     }
-    // })
 }
 
 module.exports = { makeRequest_Caso }
