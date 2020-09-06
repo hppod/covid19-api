495a32dc18d654506209330427846b4d8088cc35
hppod <hppodgornik@gmail.com>
2020-09-05 23:45:53 -0300

    Movendo a função fetchDataAPI para fetch-api.js

---
+++ app/functions/fetch-api.js
@@ -0,0 +1,33 @@
+const got = require('got')
+
+let resReturn
+
+const fetchDataAPI = async (URL, limited) => {
+    console.log(`Requisitando dados: ${URL}`)
+
+    try {
+        let { body } = await got(URL, { responseType: 'json' })
+
+        if (body['results'] == undefined) {
+            const seconds = body['available_in'].charAt(0) * 1000
+            helpers.setMyTimeout(seconds)
+            await fetchDataAPI(URL)
+        }
+
+        if (!limited) {
+            resReturn = {
+                nextUrl: body['next'],
+                data: body['results'],
+                count: Math.ceil(body['count'] / 1000)
+            }
+            return resReturn
+        }
+
+        return body['results'][0]['date']
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+module.exports = { fetchDataAPI }
--- app/job/caso/caso.js
+++ app/job/caso/caso.js
@@ -1,65 +1,38 @@
 const db = require('./db_caso')
 const helpers = require('./../../helpers/helpers')
+const requests = require('./../../functions/fetch-api')
 const { endpoint_caso, endpoint_caso_islast } = require('../endpoints.consts')
-const got = require('got')
 
-let resReturn, totalDataInserted, countPage
-
-const fetchDataAPI = async (URL, limited) => {
-    console.log(`Requisitando dados: ${URL}`)
-
-    try {
-        let { body } = await got(URL, { responseType: 'json' })
-
-        if (body['results'] == undefined) {
-            const seconds = body['available_in'].charAt(0) * 1000
-            helpers.setMyTimeout(seconds)
-            await fetchDataAPI(URL)
-        }
-
-        if (!limited) {
-            resReturn = {
-                nextUrl: body['next'],
-                data: body['results']
-            }
-            return resReturn
-        }
-
-        countPage = Math.ceil(body['count'] / 1000)
-
-        return body['results'][0]['date']
-    } catch (e) {
-        console.log(e)
-    }
-
-}
+let totalDataInserted, countPage
 
 const fetchDataAddDB = async (dataset) => {
     while (countPage > 0) {
         const dataInserted = await db.insertDataInDb(dataset['data'])
 
-        console.log(`${dataInserted} novos registros foram inseridos no banco de dados`)
-
         totalDataInserted = totalDataInserted + dataInserted
+        console.log(`Até o momento foram inseridos ${totalDataInserted} novos registros`)
+
         countPage = countPage - 1
+        console.log(`Faltam ${countPage} páginas para finalizar a rotina`)
 
         if (countPage > 0) {
             helpers.setMyTimeout(3000)
-            dataset = await fetchDataAPI(dataset['nextUrl'], false)
+            dataset = await requests.fetchDataAPI(dataset['nextUrl'], false)
         }
     }
 }
 
 async function main() {
-    console.log('Iniciando a entidade Caso')
+    console.log('Iniciando a rotina Caso')
     totalDataInserted = 0
     countPage = 0
 
-    const countDataCasoTable = await db.checkIfDbIsPopulated()
+    // const countDataCasoTable = await db.checkIfDbIsPopulated()
+    const countDataCasoTable = 1
     const isLast = countDataCasoTable > 0 ? true : false
     const URL = countDataCasoTable > 0 ? endpoint_caso_islast : endpoint_caso
 
-    let responseAPI = await fetchDataAPI(URL, true)
+    let responseAPI = await requests.fetchDataAPI(URL, true)
 
     if (isLast) {
         const dateFirstResult = responseAPI
@@ -69,11 +42,12 @@ async function main() {
             console.log('Não é necessário novas ações no banco de dados')
         } else {
             await db.updateOldDataInDb()
-            responseAPI = await fetchDataAPI(URL, false)
+            responseAPI = await requests.fetchDataAPI(URL, false)
+            countPage = responseAPI['count']
             await fetchDataAddDB(responseAPI)
         }
     } else {
-        responseAPI = await fetchDataAPI(URL, false)
+        responseAPI = await requests.fetchDataAPI(URL, false)
         await fetchDataAddDB(responseAPI)
     }
 
@@ -81,7 +55,7 @@ async function main() {
         console.log(`Ao total foram inseridos ${totalDataInserted} novos registros`)
     }
 
-    console.log('Finalizando a entidade Caso')
+    console.log('Finalizando a rotina Caso')
 }
 
 module.exports = { main }
