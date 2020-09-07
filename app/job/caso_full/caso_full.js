00bd7f113e02b6b0352c2372b7a46badfc8d90d8
hppod <hppodgornik@gmail.com>
2020-09-07 16:10:18 -0300

    Adicionando logger em CasoFull

--- app/job/caso_full/caso_full.js
+++ app/job/caso_full/caso_full.js
@@ -1,6 +1,7 @@
 const db = require('./db_caso_full')
 const helpers = require('./../../helpers/helpers')
 const requests = require('./../../functions/fetch-api')
+const logger = require('./../../log/log')
 const { endpoint_casofull } = require('./../endpoints.consts')
 
 let totalDataInserted, countPage
@@ -9,6 +10,7 @@ const fetchDataAddDB = async (dataset, timeout = 5000) => {
     while (countPage > 0) {
         const dataInserted = await db.insertDataInDb(dataset['data'])
 
+        logger.log('CSF', 'C', dataInserted)
         totalDataInserted = totalDataInserted + dataInserted
         console.log(`Até o momento foram inseridos ${totalDataInserted} novos registros`)
 
@@ -23,6 +25,7 @@ const fetchDataAddDB = async (dataset, timeout = 5000) => {
 }
 
 async function main() {
+    logger.log('CSF', 'G')
     console.log('Iniciando a rotina CasoFull')
     totalDataInserted = 0
     countPage = 0
@@ -32,6 +35,7 @@ async function main() {
     const countDataCasoFullTableByDate = await db.searchIfDataExistsByDate(responseAPI)
 
     if (countDataCasoFullTableByDate > 0) {
+        logger.log('CSF', 'C')
         console.log('Não é necessário novas ações no banco de dados')
     } else {
         await db.updateOldDataInDb()
@@ -41,6 +45,7 @@ async function main() {
     }
 
     if (totalDataInserted > 0) {
+        logger.log('CSF', 'CT', totalDataInserted)
         console.log(`Ao total foram inseridos ${totalDataInserted} novos registros`)
     }
 
